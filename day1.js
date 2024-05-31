0b0a29eb4accec583c62da2400d02c6b1587dc14
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 21:13:56 +0500

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,10 @@
+// ===========================            day 1                   ========================
+//  ============ question 2
+var NAME = "nimra";
+var message = "hello nimra would you like to learn some typescript today ?";
+console.log("hello ".concat(NAME, " would you like to learn some typescript today ?\""));
+// ============ question 3
+var Name = "nimranisa";
+console.log(Name.toLocaleLowerCase());
+console.log(Name.toLocaleUpperCase());
+console.log(Name.charAt(0).toLocaleUpperCase() + Name.slice(1).toLocaleLowerCase());
---
+++ day1.ts
@@ -0,0 +1,14 @@
+// ===========================            day 1                   ========================
+//  ============ question 2
+
+let NAME : string = "nimra";
+let message : string = "hello nimra would you like to learn some typescript today ?"
+console.log(`hello ${NAME} would you like to learn some typescript today ?"`);
+
+// ============ question 3
+
+let Name = "nimranisa"
+ console.log(Name.toLocaleLowerCase());
+ console.log(Name.toLocaleUpperCase());
+ console.log(Name.charAt(0).toLocaleUpperCase() + Name.slice(1).toLocaleLowerCase());
+ 
