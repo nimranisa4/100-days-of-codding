2d6a68ab333afa1c84658db0f0e21977ba82cb70
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 21:14:26 +0500

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,12 @@
+//============================                      day 2               ========================
+//============ question 4
+var famousPerson = "albert einstein";
+console.log("\"".concat(famousPerson, " once said, \" a person who never made a mistake never tried anythng new\"\" "));
+//============== question 5
+var famous_person = "albert einstein ";
+var message = ("\"".concat(famous_person, " once said, \" a person who never made a mistake never tried anythng new\"\""));
+console.log(message);
+//=============== question 6
+var Name = "\t\n nimranisa \t\n";
+console.log(Name);
+console.log(Name.trim());
---
+++ day2.ts
@@ -0,0 +1,20 @@
+//============================                      day 2               ========================
+//============ question 4
+ 
+let famousPerson : string = "albert einstein"
+console.log(`"${famousPerson} once said, " a person who never made a mistake never tried anythng new"" `);
+
+//============== question 5
+
+let famous_person: string = "albert einstein "  
+let message = (`"${famous_person} once said, " a person who never made a mistake never tried anythng new""`);
+console.log(message);
+
+//=============== question 6
+
+let Name : string = "\t\n nimranisa \t\n";
+console.log(Name);
+console.log(Name.trim());
+
+
+
