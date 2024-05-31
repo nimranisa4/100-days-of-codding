f17b7bcf3f20e7aa19316465beaf25e10b914f7c
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 21:14:58 +0500

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,14 @@
+//============================                     day 3                   ===================================
+// ================= question 7
+var NUMBER = 8;
+console.log(4 + 4);
+console.log(16 - 8);
+console.log(4 * 2);
+console.log(16 / 2);
+//=============== question no 8
+var number = 8;
+console.log(5 + 3);
+//================ question no 9
+var favouriteNumber = 7;
+var message = ("\"my favourite number is\" ".concat(favouriteNumber, " ."));
+console.log(message);
---
+++ day3.ts
@@ -0,0 +1,21 @@
+//============================                     day 3                   ===================================
+// ================= question 7
+
+let NUMBER = 8;
+console.log(4 + 4);
+console.log( 16 - 8 );
+console.log(4 * 2);
+console.log(16 / 2);
+
+//=============== question no 8
+
+let number = 8
+console.log(5 + 3);
+
+//================ question no 9
+
+let favouriteNumber : number = 7;
+let message = (`"my favourite number is" ${favouriteNumber} .`)
+console.log(message);
+
+
